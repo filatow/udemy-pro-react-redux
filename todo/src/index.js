2e8d1278f6a73bb742475726a132b41d834fc6b9
evgeniy <not-found@inbox.ru>
2020-10-09 12:47:52 +0300

    Созданы первый компоненты для приложения Todo

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,1 +1,21 @@
-console.log(`Hello World!`);
+import React from 'react';
+import ReactDOM from 'react-dom';
+import AppHeader from './components/app-header'
+import TodoList from './components/todo-list'
+import SearchPanel from './components/search-panel';
+
+const App = () => {
+
+  return (
+    <div>
+      <AppHeader />
+      <SearchPanel />
+      <TodoList />
+    </div>
+  )
+}
+
+
+ReactDOM.render(
+  <App />, document.getElementById(`root`)
+)
